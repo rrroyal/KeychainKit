38b2718523c0c9053c204bf0dfea4d91863bbcd2
rrroyal <royal@shameful.xyz>
2023-06-22 22:03:38 +0200

    Refactor item saving

--- Sources/KeychainKit/Keychain.swift
+++ Sources/KeychainKit/Keychain.swift
@@ -10,14 +10,14 @@ import Security
 
 public final class Keychain {
 	private typealias QueryDictionary = [CFString: Any]
+	
+	public static let defaultItemClass = kSecClassGenericPassword as String
 
-	let accessGroup: String
+	public let accessGroup: String
+	// public var applicationTagPrefix: String?
 
 	private let baseQuery: QueryDictionary
 	private let textEncoding: String.Encoding = .utf8
-
-	private let _kSecAttrDescription = "Harbour - Token"
-	private let _kSecClass = kSecClassGenericPassword
 
 	// MARK: - init
 
@@ -31,9 +31,6 @@ public final class Keychain {
 			kSecAttrAccessGroup: accessGroup,
 			kSecAttrSynchronizable: true,
 			kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
-			// kSecClass: kSecClassInternetPassword
-			kSecClass: _kSecClass
-//			kSecAttrService: service
 		]
 	}
 
@@ -42,8 +39,13 @@ public final class Keychain {
 	/// Stores provided content in keychain under specified service URL.
 	/// - Parameters:
 	///   - content: Content to store
-	///   - server: Service URL
-	public func saveContent(_ content: String, for url: URL) throws {
+	///   - url: Service URL
+	///   - itemDescription: Keychain item description
+	///   - itemClass: Keychain item class
+	public func saveContent(_ content: String,
+							for url: URL,
+							itemDescription: String? = nil,
+							itemClass: String = kSecClassGenericPassword as String) throws {
 		var query = baseQuery
 		// query[kSecAttrServer] = url.absoluteString
 		query[kSecAttrService] = url.absoluteString
@@ -56,21 +58,26 @@ public final class Keychain {
 			// kSecAttrComment: comment as Any,
 			// kSecAttrPath: url.path,
 			kSecAttrLabel: url.absoluteString,
-			kSecAttrDescription: _kSecAttrDescription
+			kSecAttrDescription: itemDescription as Any,
+			kSecClass: itemClass,
+			// kSecAttrApplicationTag: applicationTag(for: url)
 		]
 
 		try addOrUpdate(query: query, attributes: attributes)
 	}
 
 	/// Retrieves content for specified service URL..
-	/// - Parameter server: Service URL
+	/// - Parameters:
+	///   - url: Service URL
+	///   - itemClass: Keychain item class
 	/// - Returns: Saved content
-	public func getContent(for url: URL) throws -> String {
+	public func getContent(for url: URL, itemClass: String = Keychain.defaultItemClass) throws -> String {
 		var query = baseQuery
 		// query[kSecAttrServer] = url.absoluteString
 		query[kSecAttrService] = url.absoluteString
 		query[kSecMatchLimit] = kSecMatchLimitOne
 		query[kSecReturnData] = true
+		query[kSecClass] = itemClass
 
 		var item: CFTypeRef?
 		let status = SecItemCopyMatching(query as CFDictionary, &item)
@@ -87,23 +94,29 @@ public final class Keychain {
 	}
 
 	/// Deletes content for supplied service URL.
-	/// - Parameter server: Service URL
-	public func removeContent(for url: URL) throws {
+	/// - Parameters:
+	///   - url: Service URL
+	///   - itemClass: Keychain item class
+	public func removeContent(for url: URL, itemClass: String = Keychain.defaultItemClass) throws {
 		var query = baseQuery
 		// query[kSecAttrServer] = url.absoluteString
 		query[kSecAttrService] = url.absoluteString
+		// query[kSecAttrApplicationTag] = applicationTag(for: url)
+		query[kSecClass] = itemClass
+
 		let status = SecItemDelete(query as CFDictionary)
 		guard status == errSecSuccess || status == errSecItemNotFound else { throw SecError(status) }
 	}
 
 	/// Returns all saved URLs.
+	/// - Parameter itemClass: Keychain item class
 	/// - Returns: Array of URLs
-	public func getSavedURLs() throws -> [URL] {
+	public func getSavedURLs(itemClass: String = Keychain.defaultItemClass) throws -> [URL] {
 		var query = baseQuery
-//		query[kSecAttrDescription] = Self.tokenItemDescription
 		query[kSecMatchLimit] = kSecMatchLimitAll
 		query[kSecReturnAttributes] = true
 		query[kSecReturnData] = false
+		query[kSecClass] = itemClass
 
 		var item: CFTypeRef?
 		let status = SecItemCopyMatching(query as CFDictionary, &item)
@@ -134,15 +147,27 @@ public final class Keychain {
 		let addStatus = SecItemAdd(addQuery as CFDictionary, nil)
 
 		switch addStatus {
-			case errSecSuccess:
-				return
-			case errSecDuplicateItem:
-				let updateStatus = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
-				guard updateStatus == errSecSuccess else {
-					throw SecError(updateStatus)
-				}
-			default:
-				throw SecError(addStatus)
+		case errSecSuccess:
+			return
+		case errSecDuplicateItem:
+			let updateStatus = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+			guard updateStatus == errSecSuccess else {
+				throw SecError(updateStatus)
+			}
+		default:
+			throw SecError(addStatus)
 		}
 	}
+	
+	/*
+	/// Generates item tag for specified URL.
+	/// - Parameter url: Item service URL
+	/// - Returns: Item tag
+	private func applicationTag(for url: URL) -> String {
+		if let applicationTagPrefix {
+			return "\(applicationTagPrefix).\(url.absoluteString)"
+		}
+		return url.absoluteString
+	}
+	*/
 }
